32daafee6c0aaac734b78840387f4d6bf7b72e42
sumit kumar <pksumitkumar98@gmail.com>
2023-11-06 01:18:40 +0530

    add

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,13 +1,26 @@
-import { Button, Checkbox, Col, Row, Select } from 'antd'
-import React from 'react'
+import { Button, Checkbox, Col, Row, Select,Modal, Upload } from 'antd'
+import React, { useState } from 'react'
 import  "../../styles/dashboard.css"
 import CheckingAccount from "../charts/checking_account"
 import InvoiceOwn from '../charts/invoice_own'
 import AmountInOut from "../charts/amount_in_out"
 import AccountWishlist from '../charts/account_wishlist'
+import { UploadOutlined } from '@ant-design/icons';
 
 
 function Index() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
    <Row className='dashboard' justify={'space-evenly'} >
     <Col  className='card_section' md={11} span={23}>
@@ -40,9 +53,17 @@ function Index() {
     <Col  className='card_section' md={11} span={23}>
     <div className='cards_top_section'>
         Invoices Owner to you
-       <Button style={{background:'#E8EEFD',color:'#90C2BC',fontWeight:600}}>
+       <Button onClick={showModal} style={{background:'#E8EEFD',color:'#90C2BC',fontWeight:600}}>
            New Sales Invoice
        </Button>
+
+       <Modal  title="Upload File" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+       <Upload >
+    <Button icon={<UploadOutlined />}>Click to Upload</Button>
+  </Upload>
+      </Modal>
+
+
     </div>
    <InvoiceOwn /> 
     </Col>
